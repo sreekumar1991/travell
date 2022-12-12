c152bfdcf401fd0b20640397856097939ebe200a
sreekumar1991 <sreekumarpksreezz@gmail.com>
2022-12-12 12:20:59 +0530

    travel

---
+++ travelproject/travelapp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# Create your models here.
+class place(models.Model):
+    name=models.CharField(max_length=250)
+    image=models.ImageField(upload_to='pics')
+    description=models.TextField()
+
+    def __str__(self):
+        return self.name
---
+++ travelproject/travelapp/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from.models import place
+
+# Create your views here.
+def demo(request):
+    obj=place.objects.all()
+    return render(request,"index.html",{'result':obj})
+#def about(request):
+   # return render(request,"about.html")
+#def contact(request):
+    #return HttpResponse("hello am contact")
